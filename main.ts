08344eb5cb08a3bebbf6000943ad4e0484410dcf
sbillings-del <sbillings@gcsd.k12.sc.us>
2025-08-12 19:36:23 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    let mySprite: Sprite = null
+    console.log("")
+    animation.runMovementAnimation(
+    mySprite,
+    animation.animationPresets(animation.flyToCenter),
+    2000,
+    false
+    )
     music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.LoopingInBackground)
+    game.gameOver(true)
 })
